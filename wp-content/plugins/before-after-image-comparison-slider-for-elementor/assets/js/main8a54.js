ba6deac30315f861460398410abef548f850f2cf
vk <justvk2013@gmail.com>
2023-12-09 22:18:38 -0700

    refactored

--- wp-content/plugins/before-after-image-comparison-slider-for-elementor/assets/js/main8a54.js
+++ wp-content/plugins/before-after-image-comparison-slider-for-elementor/assets/js/main8a54.js
@@ -1,1 +1,47 @@
-"use strict";var $;jQuery,jQuery(window).on("load",function(){jQuery(window).trigger("resize")}),jQuery(window).on("elementor/frontend/init",function(){elementorFrontend.hooks.addAction("frontend/element_ready/wb-before-after-image-slider-elementor.default",function(e,t){var n=e.find(".before_text").text(),i=e.find(".after_text").text();e.find(".wb_ebais_twentytwenty_container").twentytwenty({before_label:n,after_label:i})})}),jQuery(window).on("scroll",function(){jQuery(".wb_ebais_twentytwenty_container").each(function(){jQuery(this).wbebaic_isInViewport()&&jQuery(window).trigger("resize")})}),jQuery.fn.wbebaic_isInViewport=function(){var e=jQuery(this).offset().top,t=e+jQuery(this).outerHeight(),n=jQuery(window).scrollTop(),i=n+jQuery(window).height();return t>n&&e<i};
+"use strict";
+
+jQuery(function ($) {
+    var resizing = false;
+
+    $(window).on("load resize", function () {
+        if (!resizing) {
+            resizing = true;
+            $(window).trigger("resize");
+            resizing = false;
+        }
+    });
+
+    $(window).on("elementor/frontend/init", function () {
+        elementorFrontend.hooks.addAction("frontend/element_ready/wb-before-after-image-slider-elementor.default", function (e, t) {
+            var $element = $(e),
+                beforeText = $element.find(".before_text").text(),
+                afterText = $element.find(".after_text").text();
+
+            $element.find(".wb_ebais_twentytwenty_container").twentytwenty({
+                before_label: beforeText,
+                after_label: afterText
+            });
+        });
+    });
+
+    $(window).on("scroll", function () {
+        $(".wb_ebais_twentytwenty_container").each(function () {
+            if ($(this).wbebaic_isInViewport()) {
+                if (!resizing) {
+                    resizing = true;
+                    $(window).trigger("resize");
+                    resizing = false;
+                }
+            }
+        });
+    });
+
+    $.fn.wbebaic_isInViewport = function () {
+        var elementTop = $(this).offset().top,
+            elementBottom = elementTop + $(this).outerHeight(),
+            viewportTop = $(window).scrollTop(),
+            viewportBottom = viewportTop + $(window).height();
+
+        return elementBottom > viewportTop && elementTop < viewportBottom;
+    };
+});
